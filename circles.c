9460b6829f3b774c9b70494170227af49b27c244
Christopher Lee <christopherlee.contato@gmail.com>
2021-08-11 20:27:21 -0300

    first commit

---
+++ circles.c
@@ -0,0 +1,67 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int tam;
+
+char screen [800][800];
+
+void clear (){
+    for (int i = 0; i < tam; i++){
+        for (int j = 0; j < tam; j++){
+            screen [i][j] = ' ';
+        }
+    }
+}
+
+void printCircleOnScreen(){
+    for (int y = 0; y < tam; y++){
+        for (int x = 0; x < tam; x++){
+             printf("%c",screen [x][y]);
+        }
+        printf("\n");
+    }
+}
+
+void circlePythagoras(int raio){
+    if (raio <= tam){
+        for (int y = -tam; y < tam; y++){
+            for (int x = -tam; x < tam; x++){
+                if ((x*x)+(y*y) < (raio*raio)){
+                    screen [x+((tam-1)/2)][y+((tam-1)/2)] = '@';
+                }
+            }
+        }
+    }
+}
+
+void circlePoints(int gap, int radius){
+    int x,y;
+    for (int i = 0; i <= 360; i+=1){
+        x = ( cos(i)* radius ) + ((tam)/2);
+        y = ( sin(i)* radius ) + ((tam)/2);
+        screen [x][y] = '#';
+    }
+}
+
+
+int main(){
+    int radius;
+    char cmd[99];
+    printf("Radius: ");
+    scanf("%d",&radius);
+    tam = radius*2;
+    clear();
+    circlePythagoras(radius);
+    printCircleOnScreen();
+
+    printf("\n\n\n");
+
+    clear();
+    circlePoints(3, radius);
+    printCircleOnScreen();
+
+    //scanf(" %[^\n]s",&cmd);
+    //system (cmd);
+    return main();
+}
